990a1c57b85999246d9377f28aadc414d873cdc5
Ruslan <trybukhouskij@gmail.com>
2024-01-28 18:37:15 +0300

    feat: implement task getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -132,9 +132,16 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length !== 0) {
+    return (
+      Math.ceil((arr.reduce((acc, el) => acc + el, 0) / arr.length) * 100) / 100
+    );
+  }
+  return 0;
 }
+
+console.log(getAverage([-1, 1, -1, 1]));
 
 /**
  * Checks if all strings in an array have the same length.
